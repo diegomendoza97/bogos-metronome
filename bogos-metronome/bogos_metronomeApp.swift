be8e57d9351cd3dd1df4f7992d265dd6f91c09b6
Diego Mendoza <diegocoronado_@outlook.com>
2023-04-09 16:14:36 -0700

    Added metronome features added later

---
+++ bogos-metronome/bogos_metronomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  bogos_metronomeApp.swift
+//  bogos-metronome
+//
+//  Created by Diego Mendoza on 3/20/23.
+//
+
+import SwiftUI
+
+@main
+struct bogos_metronomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
